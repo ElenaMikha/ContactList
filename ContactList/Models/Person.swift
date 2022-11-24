64d61c46f9803f53ac518d77339371434bb5e5c9
Elena <elmikhailova86@gmail.com>
2022-11-24 09:20:57 +0300

    some changes

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -7,15 +7,22 @@
 
 
 
+
 struct PersonData {
     
-    let names = DataStore.shared.names
-    let surnames = DataStore.shared.surnames
-    let emails = DataStore.shared.emails
-    let phones = DataStore.shared.phones
+    let name: String
+    let surname: String
+    let email: String
+    let phone: String
+    
+    
+//    let names = DataStore.shared.names
+//    let surnames = DataStore.shared.surnames
+//    let emails = DataStore.shared.emails
+//    let phones = DataStore.shared.phones
     
     var fullName: String {
-        "\(names) \(surnames)"
+        "\(name) \(surname)"
     }
     
 }
@@ -25,9 +32,11 @@ extension PersonData {
         var personDatas: [PersonData] = []
         for _ in 1...10 {
             let personData = PersonData(
-                
-            
-            )
+                name: DataStore.shared.names.randomElement() ?? "",
+                surname: DataStore.shared.surnames.randomElement() ?? "",
+                email: DataStore.shared.emails.randomElement() ?? "",
+                phone: DataStore.shared.phones.randomElement() ?? ""
+                )
            personDatas.append(personData)
         }
         return personDatas
--- ContactList/Services/DataStore.swift
+++ ContactList/Services/DataStore.swift
@@ -9,21 +9,10 @@
 
 class DataStore {
     
-    let names: [String]
-    let surnames: [String]
-    let phones: [String]
-    let emails: [String]
+    static let shared = DataStore()
     
-    init(names: [String], surnames: [String], phones: [String], emails: [String]) {
-        self.names = names
-        self.surnames = surnames
-        self.phones = phones
-        self.emails = emails
-    }
-    
-    static let shared = DataStore(
-        names: ["Sharon", "John", "Aaron", "Steven", "Nicola", "Ted", "Bruce", "Carl", "Allan", "David"],
-        surnames: ["Murphy", "Black", "Tompson", "Jankin", "Williams", "Robertson", "Butler", "Isaacson", "Smith", "Pennyworth",],
-        phones: ["11111111", "22222222", "33333333", "44444444", "5555555", "8888888", "66666666", "7777777", "999999999", "1234455566"],
-        emails: ["qwe@gg.ru", "assdd@ssd.ru", "zxxccv@sssd.ru", "gghhhh@sssd.ru", "hhggcv@sssd.ru", "qwe@gg.ru", "ttttwe@gg.ru", "jjjjjwe@gg.ru", "dddddwe@gg.ru", "kkkkkwe@gg.ru"])
+    let names = ["Sharon", "John", "Aaron", "Steven", "Nicola", "Ted", "Bruce", "Carl", "Allan", "David"]
+    let surnames = ["Murphy", "Black", "Tompson", "Jankin", "Williams", "Robertson", "Butler", "Isaacson", "Smith", "Pennyworth"]
+    let phones = ["11111111", "22222222", "33333333", "44444444", "5555555", "8888888", "66666666", "7777777", "999999999", "1234455566"]
+    let emails =  ["qwe@gg.ru", "assdd@ssd.ru", "zxxccv@sssd.ru", "gghhhh@sssd.ru", "hhggcv@sssd.ru", "qwe@gg.ru", "ttttwe@gg.ru", "jjjjjwe@gg.ru", "dddddwe@gg.ru", "kkkkkwe@gg.ru"]
 }
