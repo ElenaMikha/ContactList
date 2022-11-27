e78722a2d043fc1c0aca699d7316b418973a95f9
Elena <elmikhailova86@gmail.com>
2022-11-27 19:23:24 +0300

    some changes

--- ContactList/Services/DataStore.swift
+++ ContactList/Services/DataStore.swift
@@ -15,4 +15,6 @@ class DataStore {
     let surnames = ["Murphy", "Black", "Tompson", "Jankin", "Williams", "Robertson", "Butler", "Isaacson", "Smith", "Pennyworth"]
     let phones = ["11111111", "22222222", "33333333", "44444444", "5555555", "8888888", "66666666", "7777777", "999999999", "1234455566"]
     let emails =  ["qwe@gg.ru", "assdd@ssd.ru", "zxxccv@sssd.ru", "gghhhh@sssd.ru", "hhggcv@sssd.ru", "qwe@gg.ru", "ttttwe@gg.ru", "jjjjjwe@gg.ru", "dddddwe@gg.ru", "kkkkkwe@gg.ru"]
+    init(){}
 }
+
